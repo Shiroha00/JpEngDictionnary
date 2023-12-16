27d339f29c75d18d5b03dee87bb913ede31d8d32
Shiroha00 <hoanghiepfl1@gmail.com>
2023-12-16 14:49:39 +0700

    test1

--- app/src/main/java/com/example/jpengdictionnary/JishoApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jpengdictionnary
-
-import retrofit2.http.GET
-import retrofit2.http.Query
-
-interface JishoApiService {
-    @GET("words")
-    suspend fun searchWords(@Query("keyword") keyword: String): ApiResponse
-}
